f6a6c3d1c4409225176d72ae735c522f8705d623
Gintare Dhavde <omkardhawde@MacBook-Air.local>
2022-10-18 21:51:28 +0100

    train stops homework

--- train_stops.py
+++ train_stops.py
@@ -2,21 +2,25 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 print(stops)
 stops.append("Edinburgh Wawerley")
 print(stops)
+a="Glasgow Queen St"
+stops.insert(0, a)
+print(stops)
+b= "Polmont"
+stops.insert(2, b)
+print(stops)
+Linlithgow = 3
+print(3)
+stops.remove("Livingston")
+print(stops)
+stops.pop(1)
+num_of_stops=stops
+len(stops)
+print(stops)
+sort_stops = sorted (stops)
+print(sort_stops)
+sort_stops = sorted(stops, reverse = True)
+print(sort_stops)
 
-# #2. Add "Glasgow Queen St" to the start of the list
-# stops.insert("Glasgow Queen St")
-# #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
-# stops[2].insert("Polmont")
-# #4. Print out the index position of "Linlithgow"
-# print(stops[3])
-# #5. Remove "Livingston" from the list using its name
-# stops.remove("Livingston")
-# #6. Delete "Cumbernauld" from the list by index
-# del(stops["Cumbernauld"])
-# #7. Print the number of stops there are in the list
-# num_of_stops=stops
-# len(stops)
-# print(stops)
-# #8. Sort the list alphabetically
-# stops.sort (reverse false)
-# print (stops)
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+for stop in stops: 
+    print(stop)
