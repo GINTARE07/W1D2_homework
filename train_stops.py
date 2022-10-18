c56c369071ab88f9a32c3a58511ad2d0182717e6
Gintare Dhavde <omkardhawde@MacBook-Air.local>
2022-10-18 20:19:07 +0100

    train stations

---
+++ train_stops.py
@@ -0,0 +1,22 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+print(stops)
+stops.append("Edinburgh Wawerley")
+print(stops)
+
+# #2. Add "Glasgow Queen St" to the start of the list
+# stops.insert("Glasgow Queen St")
+# #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+# stops[2].insert("Polmont")
+# #4. Print out the index position of "Linlithgow"
+# print(stops[3])
+# #5. Remove "Livingston" from the list using its name
+# stops.remove("Livingston")
+# #6. Delete "Cumbernauld" from the list by index
+# del(stops["Cumbernauld"])
+# #7. Print the number of stops there are in the list
+# num_of_stops=stops
+# len(stops)
+# print(stops)
+# #8. Sort the list alphabetically
+# stops.sort (reverse false)
+# print (stops)
